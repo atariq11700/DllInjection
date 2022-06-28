3ef41eb9fe160848f3777b889fa525535d17815b
Adam Alder <atariq11700@gmail.com>
2022-06-28 16:09:35 -0600

    wait 2 sec before exiting

--- DllSource/dllmain.cpp
+++ DllSource/dllmain.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <windows.h>
+#include <time.h>
 
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved ) {
     switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH: {
-            // printf("Hello From DLL\n");
             std::cout << "Hello From DLL\n";
+            Sleep(2000);
             exit(0);
         };
         break;
